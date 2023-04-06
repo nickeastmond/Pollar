ec9395eec1b30d195b0a74c26bfdff08f8aea9df
nickeastmond <nicholas.eastmond@gmail.com>
2023-04-06 11:06:14 -0700

    UI for Signup, error messages, and Snack Bars

--- lib/login/firebase_user_login.dart
+++ lib/login/firebase_user_login.dart
@@ -1,24 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class FirebaseLogin {
-    // If you are listening to changes in authentication state, 
-    // a new event will be sent to your listeners if succesful.
-    static Future<void> firebaseUserLogin(String emailAddress, String password) async {
-      try {
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: emailAddress,
-          password: password
-        );
-        print("Successfully signed in");
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'user-not-found') {
-          print('No user found for that email.');
-        } else if (e.code == 'wrong-password') {
-          print('Wrong password provided for that user.');
-        }
+  // If you are listening to changes in authentication state,
+  // a new event will be sent to your listeners if succesful.
+  static Future<String> firebaseUserLogin(
+      String emailAddress, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
+      debugPrint("Successfully signed in");
+      return '';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        debugPrint('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        debugPrint('Wrong password provided for that user.');
       }
-      
-
+      return ("Email or password is incorrect, please type credentials again.");
     }
-      
+  }
 }
