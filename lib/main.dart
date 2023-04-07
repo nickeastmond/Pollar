488c06b79d51e5e2b959fb3f3b9c3f87a74b49bf
Sarah <sarahthi283@gmail.com>
2023-04-06 20:07:06 -0700

    permhandler import

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pollar/services/shared_preferences_service.dart';
 import 'package:pollar/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
-import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 void main() async {
   //DO NOT EDIT
