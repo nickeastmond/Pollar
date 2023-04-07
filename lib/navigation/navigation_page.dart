a47e995e8e53f022e5014b7d2d8dfec02fa4768e
Sarah <sarahthi283@gmail.com>
2023-04-06 19:04:12 -0700

    temp signout button added in bottom navbar & sign in takes user to nav page

--- lib/navigation/navigation_page.dart
+++ lib/navigation/navigation_page.dart
@@ -1,6 +1,7 @@
 //  Created by Nicholas Eastmond on 9/26/22.
 
 import 'package:flutter/material.dart';
+import 'package:pollar/login/login_page.dart';
 
 import '../polls_theme.dart';
 
@@ -75,6 +76,18 @@ class NavigationPageState extends State<NavigationPage> {
                   size: iconSize,
                 ),
                 label: 'Profile Page',
+              ),
+
+              // TEMP
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  icon: const Icon(Icons.exit_to_app),
+                  iconSize: iconSize,
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginPage()), (route) => false);
+                  }
+                  ),
+                label: 'Temporary Sign Out Page',
               ),
             ],
           ),
