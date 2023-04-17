2ebbb9c551ccbd09217aed21accfddb779d26e02
daringh10 <darin.gharib@outlook.com>
2023-04-17 14:02:01 -0700

    changed get userbyid

--- lib/model/user/database/get_user_db.dart
+++ lib/model/user/database/get_user_db.dart
@@ -1,22 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-Future<List<QueryDocumentSnapshot>> getUserById(String uid) async {
-    var firestore = FirebaseFirestore.instance; 
-    CollectionReference ref = firestore.collection('User');
-    // Get a reference to the Firestore database
-    // Query for a document with matching ID
-    final querySnapshot = await ref
-        .where('id', isEqualTo: uid)
-        .get();
-    if (querySnapshot.docs.isEmpty) {
-      print('No matching document found');
-    } else {
-      querySnapshot.docs.forEach((doc) {
-        print('Document with ID ${doc.id} found');
-        
-      });
-  }
-  return querySnapshot.docs;
-     
+Future<DocumentSnapshot<Object?>> getUserById(String uid) async {
+
+  DocumentReference documentReference = FirebaseFirestore.instance.collection('User').doc(uid);
+  DocumentSnapshot documentSnapshot = await documentReference.get();
+  return documentSnapshot;
 } 
 
 Future<List<QueryDocumentSnapshot>> getUserByEmail(String? emailAddress) async {
