e855f6c6072bb22152ce75dc53c66d097df6281b
daringh10 <darin.gharib@outlook.com>
2023-04-18 10:35:51 -0700

    fixed sign in/ singout on IOS

--- lib/navigation/profile_page.dart
+++ lib/navigation/profile_page.dart
@@ -234,13 +234,7 @@ class _ProfilePageState extends State<ProfilePage> {
                                 fontSize: 17,
                               )),
                           onPressed: () async {
-                            PollarAuth.signOut().then((_) {
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const LoginPage()),
-                              );
-                            });
+                            PollarAuth.signOut();
                           },
                         ),
                       ],
