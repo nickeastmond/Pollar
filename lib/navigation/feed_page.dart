c4253e6c27fa896798a277d01f458661c642fb7c
daringh10 <darin.gharib@outlook.com>
2023-04-25 13:30:22 -0700

    added constans file

--- lib/navigation/feed_page.dart
+++ lib/navigation/feed_page.dart
@@ -10,13 +10,13 @@ import 'package:pollar/model/Poll/poll_model.dart';
 import 'package:pollar/model/Position/position_adapter.dart';
 import 'package:pollar/model/user/pollar_user_model.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import '../model/Poll/database/delete_all.dart';
 import '../polls/poll_card.dart';
 import '../polls_theme.dart';
 
-const double MAX_DISTANCE = 5.0; // MILES
 
+const double RADIUS = 5.0; // MILES
 class PollFeedObject {
   Poll poll;
   String pollId;
@@ -30,122 +30,108 @@ class FeedProvider extends ChangeNotifier {
 
   List<PollFeedObject> get items => _items;
 
+
+
+
+
   double latIncrement(userLat, userLong) {
     double latitude = 40.7128; // New York City's latitude in degrees
-    double miles = MAX_DISTANCE; // the distance to convert, in miles
+    double miles = RADIUS; // the distance to convert, in miles
     double earthRadius = 3963.1906; // earth radius in miles
-    double degreeLatLength = earthRadius *
-        pi /
-        180; // length of one degree of latitude at equator in miles
-    double degreeLatLengthAtGivenLat = degreeLatLength *
-        cos(latitude *
-            pi /
-            180); // length of one degree of latitude at given latitude
-    double latIncrement =
-        miles / degreeLatLengthAtGivenLat; // latitude increment in degrees
+    double degreeLatLength = earthRadius * pi / 180; // length of one degree of latitude at equator in miles
+    double degreeLatLengthAtGivenLat = degreeLatLength * cos(latitude * pi / 180); // length of one degree of latitude at given latitude
+    double latIncrement = miles / degreeLatLengthAtGivenLat; // latitude increment in degrees
     return latIncrement;
   }
 
-  double longIncrement(userLat, userLong) {
+   double longIncrement(userLat, userLong) {
     double latitude = 40.7128; // New York City's latitude in degrees
-    double miles = MAX_DISTANCE; // the distance to convert, in miles
+    double miles = RADIUS; // the distance to convert, in miles
     double earthRadius = 3963.1906; // earth radius in miles
-    double degreeLatLength = earthRadius *
-        pi /
-        180; // length of one degree of latitude at equator in miles
-    double degreeLatLengthAtGivenLat = degreeLatLength *
-        cos(latitude *
-            pi /
-            180); // length of one degree of latitude at given latitude
-    double latIncrement =
-        miles / degreeLatLengthAtGivenLat; // latitude increment in degrees
-    double lonIncrement = latIncrement *
-        cos(latitude * pi / 180); // longitude increment in degrees
+    double degreeLatLength = earthRadius * pi / 180; // length of one degree of latitude at equator in miles
+    double degreeLatLengthAtGivenLat = degreeLatLength * cos(latitude * pi / 180); // length of one degree of latitude at given latitude
+    double latIncrement = miles / degreeLatLengthAtGivenLat; // latitude increment in degrees
+    double lonIncrement = latIncrement * cos(latitude * pi / 180); // longitude increment in degrees
     return lonIncrement;
   }
 
   Future<void> fetchInitial(int limit) async {
-    debugPrint("fetchin initial");
-    // Define the user's current location
+      debugPrint("fetchin initial");
+      // Define the user's current location
 
-    final position = await PositionAdapter.getFromSharedPreferences("location");
-    final double userLat = position!.latitude;
-    final double userLong = position.latitude;
-    final currentLocation = GeoPoint(userLat, userLong);
 
-    final snapshot = await FirebaseFirestore.instance
-        .collection('Poll')
-        .where('locationData',
-            isGreaterThan: GeoPoint(
-              userLat - latIncrement(userLat, userLong),
-              userLong - longIncrement(userLat, userLong),
-            ))
-        .where('locationData',
-            isLessThan: GeoPoint(
-              userLat + latIncrement(userLat, userLong),
-              userLong + longIncrement(userLat, userLong),
-            ))
-        .orderBy("locationData", descending: true)
-        .limit(limit)
-        .get();
-    _items = snapshot.docs
-        .map((doc) => PollFeedObject(Poll.fromDoc(doc), doc.id))
-        .toList();
-
-    _items.map((item) => print(item.poll.timestamp));
-
-    notifyListeners();
-  }
-
-  Future<void> fetchMore(int limit) async {
-    try {
-      final position =
-          await PositionAdapter.getFromSharedPreferences("location");
+      final position = await PositionAdapter.getFromSharedPreferences("location");
       final double userLat = position!.latitude;
       final double userLong = position.latitude;
-      final currentLocation = GeoPoint(userLat, userLong);
+      final currentLocation = GeoPoint(userLat,userLong);
 
-      print("Fetcjing more");
+
+      final snapshot = await FirebaseFirestore.instance
+        .collection('Poll')
+        .where('locationData', isGreaterThan: GeoPoint(
+  userLat - latIncrement(userLat, userLong),userLong - longIncrement(userLat, userLong),
+)).where('locationData', isLessThan: GeoPoint(
+  userLat + latIncrement(userLat, userLong),userLong + longIncrement(userLat, userLong),
+)).orderBy("locationData",descending: true)
+        .limit(limit)
+        .get();
+      _items = snapshot.docs.map((doc) => PollFeedObject(Poll.fromDoc(doc),doc.id)).toList();
+      
+      
+      _items.map((item)=>print(item.poll.timestamp));
+      
+      
+      
+      notifyListeners();
+  
+  }
+    Future<void> fetchMore(int limit) async {
+      try
+      {
+        final position = await PositionAdapter.getFromSharedPreferences("location");
+      final double userLat = position!.latitude;
+      final double userLong = position.latitude;
+      final currentLocation = GeoPoint(userLat,userLong);
+
+         print("Fetcjing more");
       final lastDocId = _items.lastWhere((item) => item != null).pollId;
-      final lastDoc = await FirebaseFirestore.instance
-          .collection("Poll")
-          .doc(lastDocId)
-          .get();
+      final lastDoc = await FirebaseFirestore.instance.collection("Poll").doc(lastDocId).get();
       final querySnapshot = await FirebaseFirestore.instance
-          .collection('Poll')
-          .where('locationData',
-              isGreaterThan: GeoPoint(
-                userLat - MAX_DISTANCE,
-                userLong - MAX_DISTANCE,
-              ))
-          .where('locationData',
-              isLessThan: GeoPoint(
-                userLat + MAX_DISTANCE,
-                userLong + MAX_DISTANCE,
-              ))
-          .orderBy("locationData", descending: true)
-          .startAfterDocument(lastDoc)
-          .limit(limit)
-          .get();
-      final newItems = querySnapshot.docs
-          .map((doc) => PollFeedObject(Poll.fromDoc(doc), doc.id))
-          .toList();
-
-      if (newItems.isEmpty) {
+        .collection('Poll')
+         .where('locationData', isGreaterThan: GeoPoint(
+  userLat - RADIUS,userLong - RADIUS,
+  
+)).where('locationData', isLessThan: GeoPoint(
+  userLat + RADIUS,userLong + RADIUS,
+)).orderBy("locationData",descending: true)
+        .startAfterDocument(lastDoc)
+        .limit(limit)
+        .get();
+    final newItems = querySnapshot.docs
+        .map((doc) => PollFeedObject(Poll.fromDoc(doc), doc.id))
+        .toList();
+      
+      if (newItems.isEmpty)
+      {
         _moreItemsToLoad = false;
-      } else {
+      }
+      else
+      {
         _moreItemsToLoad = true;
       }
+    
+    _items.addAll(newItems);
 
-      _items.addAll(newItems);
-
-      // THIS COULD GET BAD IF THERE ARE A LOT OF POLLS, WE CAN CHANGE LATER
-
-      notifyListeners();
-    } catch (e) {
-      debugPrint("No polls in database");
-      return;
-    }
+    // THIS COULD GET BAD IF THERE ARE A LOT OF POLLS, WE CAN CHANGE LATER
+    
+    notifyListeners();
+      } catch (e)
+      {
+        debugPrint("No polls in database");
+        return;
+      }
+     
+    
   }
 }
 
@@ -166,22 +152,18 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+
+  
   LatLng _userLocation = LatLng(0, 0);
   String locality = '';
   final MapController _mapController = MapController();
 
   final ScrollController _scrollController = ScrollController();
 
+
   Future<LocationData> _getCurrentLocation() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.getInt('Radius') != null) {
-      _userLocation =
-          LatLng(prefs.getDouble('Latitiude')!, prefs.getDouble('Longitude')!);
-    } else {
-      final position =
-          await PositionAdapter.getFromSharedPreferences("location");
-      _userLocation = LatLng(position!.latitude, position.longitude);
-    }
+    final position = await PositionAdapter.getFromSharedPreferences("location");
+    _userLocation = LatLng(position!.latitude, position.longitude);
     _mapController.move(_userLocation, 13);
     debugPrint("setting state to $_userLocation");
     List<Placemark> placemark = await placemarkLocation(_userLocation);
@@ -204,17 +186,17 @@ class _FeedPageState extends State<FeedPage> {
     }
   }
 
-  Future<void> _fetchMore() async {
+   Future<void> _fetchMore() async {
     // Fetch new items
     await Provider.of<FeedProvider>(context, listen: false).fetchMore(6);
-  }
-
+   }
   @override
   initState() {
     super.initState();
-
+    
     _scrollController.addListener(_onScroll);
 
+    
     // _getCurrentLocation();
   }
 
@@ -249,17 +231,20 @@ class _FeedPageState extends State<FeedPage> {
                         shrinkWrap: false,
                         itemCount: provider.items.length,
                         itemBuilder: (_, int index) {
-                          if (index == provider.items.length - 1 &&
-                              provider._moreItemsToLoad) {
-                            // declare the boolean and return loading indicator
-                            return const Center(
-                              heightFactor: 3,
-                              child: CircularProgressIndicator(),
+                          if(index == provider.items.length-1 && provider._moreItemsToLoad){
+                  // declare the boolean and return loading indicator
+                return const Center(
+                  heightFactor: 3,
+                         child: CircularProgressIndicator(
+                                  
+                                  
+                                 ),
                             );
                           }
-
+                          
+                          
                           final pollItem = provider.items[index];
-
+                          
                           if (index == 0) {
                             return Column(
                               children: [
@@ -328,8 +313,7 @@ class _FeedPageState extends State<FeedPage> {
                                   padding: const EdgeInsets.only(
                                       left: 8.0, right: 8.0, top: 8, bottom: 0),
                                   child: PollCard(
-                                    poll: pollItem,
-                                  ),
+                                      poll: pollItem,),
                                 ),
                               ],
                             );
@@ -337,7 +321,8 @@ class _FeedPageState extends State<FeedPage> {
                           return Padding(
                             padding: const EdgeInsets.only(
                                 left: 8.0, right: 8.0, top: 8, bottom: 0),
-                            child: PollCard(poll: pollItem),
+                            child: PollCard(
+                                poll:pollItem),
                           );
                         },
                       ),
