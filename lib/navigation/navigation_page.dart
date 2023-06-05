2a2ffeef503872f7ffaebfb8f9259ff2fe6a45ac
gudhillon <gudhillon@gmail.com>
2023-06-05 13:23:47 -0700

    global polling

--- lib/navigation/navigation_page.dart
+++ lib/navigation/navigation_page.dart
@@ -97,6 +97,7 @@ class NavigationPageState extends State<NavigationPage> {
                                         feedProvider: feedProvider,
                                         fromFeed: true,
                                       ),
+                                      settings: RouteSettings(arguments: runtimeType.toString()),
                                     ),
                                   );
                                 }
